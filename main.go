6ba058edadc57b1fa85c5969d33f96de7fc98ab1
Mr.A <145839706+nameIess@users.noreply.github.com>
2026-09-26 13:05:21 +0530

    rewrite: add Wails UI and app shell

--- main.go
+++ main.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"embed"
+	"log"
 
-	tea "github.com/charmbracelet/bubbletea"
-	"mklink/ui"
-	"mklink/utils"
+	"mklink/internal/linker"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
+//go:embed all:frontend/dist
+var assets embed.FS
+
 func main() {
-	// Enable ANSI colors
-	_ = utils.EnableVirtualTerminalProcessing()
-
-	if !utils.IsAdmin() {
-		fmt.Println("Requesting Administrative Privileges...")
-		if err := utils.RunAsAdmin(); err != nil {
-			fmt.Printf("Failed to elevate: %v\n", err)
-			fmt.Println("Press Enter to exit...")
-			fmt.Scanln()
-			os.Exit(1)
-		}
-		os.Exit(0)
-	}
-
-	p := tea.NewProgram(ui.InitialModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
+	app := NewApp(linker.NewService())
+	if err := wails.Run(&options.App{
+		Title:            "MK-Link",
+		Width:            960,
+		Height:          680,
+		MinWidth:         820,
+		MinHeight:        600,
+		AssetServer:      options.AssetServer{Assets: assets},
+		BackgroundColour: &options.RGBA{R: 247, G: 248, B: 250, A: 1},
+		OnStartup:        app.startup,
+		Windows:          &windows.Options{DisableWindowIcon: false},
+		Bind:             []interface{}{app},
+	}); err != nil {
+		log.Fatal(err)
 	}
 }
